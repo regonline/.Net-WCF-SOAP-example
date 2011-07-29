290a4282107802f80add355598d5bf8c9b603d01
RegOnline <brett.burch@activenetwork.com>
2011-07-29 11:17:30 -0600

    added soap client partial class for IDisposable exception protection

---
+++ RegOnlineAPISoapClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace RegOnline.ConsoleAPISample.RegOnlineAPIProxy
+{
+    /// <summary>
+    /// RegOnline API soap client
+    /// </summary>
+    public partial class RegOnlineAPISoapClient : IDisposable
+    {
+        /// <summary>
+        /// Properly disposes of the WCF client, taking exception handling 
+        /// based on State into consideration.
+        /// </summary>
+        public void Dispose()
+        {
+            if (this.State == CommunicationState.Faulted)
+            {
+                this.Abort();
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+    }
+}
